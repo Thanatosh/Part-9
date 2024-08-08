0815fb8db684602aa619f9b8eb8ff214f29f213e
Thanatos <Thanatosh@gmail.com>
2024-08-08 17:09:22 +0300

    Part 9.9 Full Commit

--- Part-9C/index.ts
+++ Part-9C/index.ts
@@ -1,8 +1,11 @@
 import express from 'express';
+const cors = require('cors');
 const app = express();
+
+app.use(cors());
 app.use(express.json());
 
-const PORT = 4000;
+const PORT = 3001;
 
 app.get('/api/ping', (_req, res) => {
   console.log('someone pinged here');
