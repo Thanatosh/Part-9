4d680b5c232273cc96f50a470d66463c955d893f
Thanatos <Thanatosh@gmail.com>
2024-08-08 17:11:32 +0300

    Part 9.9 Fix Commit

--- Part-9C/build/index.js
+++ Part-9C/build/index.js
@@ -4,9 +4,11 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const cors = require('cors');
 const app = (0, express_1.default)();
+app.use(cors());
 app.use(express_1.default.json());
-const PORT = 4000;
+const PORT = 3001;
 app.get('/api/ping', (_req, res) => {
     console.log('someone pinged here');
     res.send('pong');
